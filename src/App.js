cd7973023df22bc9c95c4cb8aeca9e85f89b4312
Josef Hardi <josef.hardi@gmail.com>
2022-09-27 21:52:12 -0700

    Hide the navbar on the dashboard page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Help from './pages/Help';
+import Dashboard from './pages/Dashboard';
+import Overview from './pages/Overview';
+import RepairIncompleteness from './pages/RepairIncompleteness';
+import RepairInconsistency from './pages/RepairInconsistency';
+
+const DefaultContainer = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
+
+const DashboardContainer = () => (
+  <Outlet />
+);
 
 const App = () => (
   <Router>
-    <Navbar />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="help" element={<Help />} />
+      <Route element={<DefaultContainer />}>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="help" element={<Help />} />
+      </Route>
+      <Route element={<DashboardContainer />}>
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route index element={<Navigate to="overview" replace />} />
+          <Route path="overview" element={<Overview />} />
+          <Route path="repair-incompleteness" element={<RepairIncompleteness />} />
+          <Route path="repair-inconsistency" element={<RepairInconsistency />} />
+        </Route>
+      </Route>
     </Routes>
+
   </Router>
 );
 
