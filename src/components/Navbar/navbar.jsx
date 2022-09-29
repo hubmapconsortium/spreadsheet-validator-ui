0d0ce081548d8aec16c531f566802a6620130de3
Josef Hardi <josef.hardi@gmail.com>
2022-09-28 21:40:41 -0700

    Have a styled component rather than CSS

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,14 +1,33 @@
-import { Stack } from '@mui/material';
+import { Stack, styled } from '@mui/material';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+
+const NavLink = styled(Link)({
+  fontSize: '14pt',
+  fontStyle: 'normal',
+  '&:link': {
+    textDecoration: 'none',
+    color: '#000000',
+  },
+  '&:visited': {
+    textDecoration: 'none',
+    color: '#000000',
+  },
+  '&:hover': {
+    textDecoration: 'none',
+  },
+  '&:active': {
+    textDecoration: 'none',
+    color: '#000000',
+  },
+});
 
 const Navbar = () => (
   <nav>
     <Stack direction="row" spacing={4} sx={{ padding: '30px' }}>
-      <Link to="about" class="link">About</Link>
-      <Link to="help" class="link">Help</Link>
+      <NavLink to="about">About</NavLink>
+      <NavLink to="help">Help</NavLink>
     </Stack>
-
   </nav>
 );
 
