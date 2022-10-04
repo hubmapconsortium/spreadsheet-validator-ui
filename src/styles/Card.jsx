70d288fa6605229ea3afc3a93e4580db9d624d8f
Josef Hardi <josef.hardi@gmail.com>
2022-10-04 09:57:44 -0700

    Update Card.jsx

--- src/styles/Card.jsx
+++ src/styles/Card.jsx
@@ -5,7 +5,7 @@ const Card = styled(Section)({
   boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
   borderRadius: '16px',
   backgroundColor: '#ffffff',
-  marginBottom: '30px',
+  marginBottom: '50px',
 });
 
 export default Card;
