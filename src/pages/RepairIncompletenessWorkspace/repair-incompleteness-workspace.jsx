8af65e3f44badf8d9da0310bda7e1dbd19e30ebd
Josef Hardi <josef.hardi@gmail.com>
2022-10-14 17:12:49 -0700

    Use the app context data to count the missing rows dynamically

--- src/pages/RepairIncompletenessWorkspace/repair-incompleteness-workspace.jsx
+++ src/pages/RepairIncompletenessWorkspace/repair-incompleteness-workspace.jsx
@@ -1,11 +1,26 @@
+import { useContext, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import AppContext from '../AppContext';
 import PageTitle from '../../components/PageTitle';
 import DefaultInfoSection from '../../components/DefaultInfoSection';
 import RepairIncompletnessEditor from '../../components/RepairIncompletenessEditor';
 import Section from '../../styles/Section';
 import { REPAIR_INCOMPLETENESS } from '../../constants/PageTitle';
 
+const countBadRows = (errorReport, inColumn) => (
+  errorReport.missingRequired[inColumn]?.length
+);
+
 const RepairIncompletenessWorkspace = () => {
-  const subtitle = '17 rows were missing sample_ID.';
+  const { errorReport } = useContext(AppContext);
+  const { column } = useParams();
+
+  const getBadRows = useMemo(
+    () => countBadRows(errorReport, column),
+    [errorReport, column],
+  );
+
+  const subtitle = `${getBadRows} rows were missing the ${column} value.`;
   return (
     <>
       <Section>
