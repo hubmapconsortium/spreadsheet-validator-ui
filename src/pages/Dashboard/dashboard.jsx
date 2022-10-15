14bbfcba36b2ef837b6ca9f1aaa50f4b0f0b8b7c
Josef Hardi <josef.hardi@gmail.com>
2022-10-14 17:09:46 -0700

    Use Context hook to broadcast the application-wide data

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -1,12 +1,16 @@
 import { Stack } from '@mui/material';
 import Sidebar from '../../components/Sidebar';
 import ContentArea from '../../components/ContentArea';
+import AppContext from '../AppContext';
+import { APP_CONTEXT } from '../../constants/TestData';
 
 const Dashboard = () => (
-  <Stack direction="row">
-    <Sidebar />
-    <ContentArea />
-  </Stack>
+  <AppContext.Provider value={APP_CONTEXT}>
+    <Stack direction="row">
+      <Sidebar />
+      <ContentArea />
+    </Stack>
+  </AppContext.Provider>
 );
 
 export default Dashboard;
