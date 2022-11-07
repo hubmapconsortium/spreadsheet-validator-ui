d1890be9e843215632fd17f4862e23d8b74dde3e
Josef Hardi <josef.hardi@gmail.com>
2022-11-07 10:37:01 -0800

    Remove dropdown icon and replace it with search itcon

--- src/components/DataSheet/SearchableSelector/searchable-selector.jsx
+++ src/components/DataSheet/SearchableSelector/searchable-selector.jsx
@@ -1,15 +1,31 @@
-import { Autocomplete, TextField } from '@mui/material';
+import { Autocomplete, InputAdornment, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 import PropTypes from 'prop-types';
 
 const SearchableSelector = ({ options, onChange, onKeyPress }) => (
   <Autocomplete
+    forcePopupIcon={false}
+    autoHighlight
     options={options}
     onChange={onChange}
     onKeyPress={onKeyPress}
     sx={{ height: '40px' }}
     getOptionLabel={(option) => option.label}
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    renderInput={(params) => <TextField {...params} size="small" />}
+    renderInput={(params) => (
+      <TextField
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...params}
+        size="small"
+        InputProps={{
+          ...params.InputProps,
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>),
+          disableUnderline: true,
+        }}
+      />
+    )}
   />
 );
 
