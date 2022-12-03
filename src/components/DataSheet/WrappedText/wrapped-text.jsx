9210f5888912a6688fb0ab7019eb17a61e03c39c
Josef Hardi <josef.hardi@gmail.com>
2022-12-02 19:42:17 -0800

    Add a tooltip for long text only

--- src/components/DataSheet/WrappedText/wrapped-text.jsx
+++ src/components/DataSheet/WrappedText/wrapped-text.jsx
@@ -5,16 +5,21 @@ const CellValue = styled(Typography)({
   fontSize: '17px',
 });
 
-const WrappedText = ({ text, color }) => (
-  <Tooltip
-    arrow
-    title={<Typography fontSize={16}>{text}</Typography>}
-    placement="right"
-    enterDelay={1000}
-  >
-    <CellValue color={color} noWrap>{text}</CellValue>
-  </Tooltip>
-);
+const WrappedText = ({ text, color }) => {
+  if (text.length <= 30) {
+    return (<CellValue color={color}>{text}</CellValue>);
+  }
+  return (
+    <Tooltip
+      arrow
+      title={<Typography fontSize={16}>{text}</Typography>}
+      placement="right"
+      enterDelay={1000}
+    >
+      <CellValue color={color} noWrap>{text}</CellValue>
+    </Tooltip>
+  );
+};
 
 WrappedText.propTypes = {
   text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
