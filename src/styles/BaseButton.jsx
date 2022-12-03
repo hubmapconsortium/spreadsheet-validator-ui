b9c0f9009bbecb2ab15d3c3aeef852465dd1618c
Josef Hardi <josef.hardi@gmail.com>
2022-12-03 08:42:57 -0800

    Update BaseButton.jsx

--- src/styles/BaseButton.jsx
+++ src/styles/BaseButton.jsx
@@ -3,9 +3,9 @@ import { Button, styled } from '@mui/material';
 const BaseButton = styled(Button)({
   borderRadius: '20px',
   paddingTop: '10px',
-  paddingLeft: '60px',
+  paddingLeft: '40px',
   paddingBottom: '10px',
-  paddingRight: '60px',
+  paddingRight: '40px',
   margin: '10px',
 });
 
