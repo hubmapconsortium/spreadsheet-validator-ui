066341261024e3b378139967268e31e0f8c97119
Josef Hardi <josef.hardi@gmail.com>
2023-03-29 18:47:34 -0400

    Remove unused variables

--- src/constants/Sheet.js
+++ src/constants/Sheet.js
@@ -1,5 +1,3 @@
 export const MAIN_SHEET = 'MAIN';
 export const METADATA_SHEET = '.metadata';
-export const CEDAR_TEMPLATE_NAME = 'schema:name';
-export const CEDAR_TEMPLATE_VERSION = 'schema:schemaVersion';
 export const CEDAR_TEMPLATE_IRI = 'pav:derivedFrom';
